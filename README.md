3f96b2d3abf63b690aa49d2eadec625772f221ba
Petr Bělohlávek <petrbel@users.noreply.github.com>
2017-05-20 10:03:32 +0200

    Refactor (#4)
    * reqs support new versions of PyPDF2
    
    * opening/closing the outputstream by with
    
    * argparse fix
    
    * simpler ordering
    
    * ignoring .idea/
    
    * examples
    
    * useless var

--- README.md
+++ README.md
@@ -10,5 +10,13 @@ Argumenty:
 
 - **-m --margin** - například 18 nebo 13; 18 funguje na slajdy z AI a snad i na ostatní od Bartáka, 13 na slajdy ze statistického zpracování jazyků
 - **-r --reverse** - mění virtuální origin pdf, místo defaultního levého dolního rohu se použije levý horní, fungovalo na ty slajdy ze statistického zpracování jazyků
-- **-fa --filename_append** - default je \_cropped
+- **-fa --filename-append** - default je \_cropped
 - **-o --order** - explicitně vypsané 1, 2, 3, 4 v pořadí, ve kterém má skript řadit stránky - 1 je levá horní část, 3 je levá dolní část 
+
+Example:
+```bash
+python crop.py sample.pdf
+python crop.py -r sample.pdf
+python crop.py -o 2 1 3 4 sample.pdf
+python crop.py -o 2 1 3 4 -r sample.pdf
+```
--- crop.py
+++ crop.py
@@ -8,17 +8,15 @@ __author__ = 'Gyfis'
 
 if __name__ == '__main__':
 
-    default_order = [1, 2, 3, 4]
-
     parser = argparse.ArgumentParser(description="Easily crop slides from your favorite teacher - BartaK!")
-    parser.add_argument('input', nargs='*', default=[], help='The files you want to convert.')
-    parser.add_argument('-m', '--margin', dest='margin', nargs='?', type=float, default=18.0,
+    parser.add_argument('input', nargs='*', help='The files you want to convert.')
+    parser.add_argument('-m', '--margin', type=float, default=18.0,
                         help='The margin for the files. 13 and 18 are quite favorite.')
-    parser.add_argument('-fa', '--filename_append', dest='filename_append', nargs='?', type=str, default='_cropped',
+    parser.add_argument('-fa', '--filename-append', default='_cropped',
                         help='The append to use for the new filenames.')
-    parser.add_argument('-r', '--reverse', dest='reverse', nargs='?', type=bool, default=False, const=True,
+    parser.add_argument('-r', '--reverse', action='store_true',
                         help='The pdf origin system is left up instead of left bottom, which is the convention.')
-    parser.add_argument('-o', '--order', dest='order', nargs='*', type=int, default=default_order,
+    parser.add_argument('-o', '--order', nargs=4, type=int, default=[1, 2, 3, 4],
                         help='Explicitly set the order of pages on the crop, in case the reverse doesn\'t help.')
 
     args = parser.parse_args()
@@ -26,8 +24,10 @@ if __name__ == '__main__':
     input_files = args.input
     margin = args.margin
     filename_append = args.filename_append
-    upside_down = args.reverse
     order = args.order
+
+    if args.reverse:
+        order = [order[i-1] for i in [3, 1, 4, 2]]
 
     width_margin = margin
     height_margin = margin
@@ -41,63 +41,52 @@ if __name__ == '__main__':
         input4 = PdfFileReader(open(input_file, 'rb'))
 
         output = PdfFileWriter()
-        output_stream = open(output_file, 'wb')
+        with open(output_file, 'wb') as output_stream:
 
-        pages = input1.getNumPages()
+            top_right = (float(input1.getPage(1).mediaBox.getUpperRight_x()) - width_margin,
+                         float(input1.getPage(1).mediaBox.getUpperRight_y()) - height_margin)
+            top_left = (float(input1.getPage(1).mediaBox.getUpperLeft_x()) + width_margin,
+                        float(input1.getPage(1).mediaBox.getUpperLeft_y()) - height_margin)
+            bottom_right = (float(input1.getPage(1).mediaBox.getLowerRight_x()) - width_margin,
+                            float(input1.getPage(1).mediaBox.getLowerRight_y()) + height_margin)
+            bottom_left = (float(input1.getPage(1).mediaBox.getLowerLeft_x()) + width_margin,
+                           float(input1.getPage(1).mediaBox.getLowerLeft_y()) + height_margin)
 
-        top_right = (float(input1.getPage(1).mediaBox.getUpperRight_x()) - width_margin,
-                     float(input1.getPage(1).mediaBox.getUpperRight_y()) - height_margin)
-        top_left = (float(input1.getPage(1).mediaBox.getUpperLeft_x()) + width_margin,
-                    float(input1.getPage(1).mediaBox.getUpperLeft_y()) - height_margin)
-        bottom_right = (float(input1.getPage(1).mediaBox.getLowerRight_x()) - width_margin,
-                        float(input1.getPage(1).mediaBox.getLowerRight_y()) + height_margin)
-        bottom_left = (float(input1.getPage(1).mediaBox.getLowerLeft_x()) + width_margin,
-                       float(input1.getPage(1).mediaBox.getLowerLeft_y()) + height_margin)
+            middle_right = (top_right[0], bottom_right[1] + (top_right[1] - bottom_right[1]) / 2)
+            middle_left = (top_left[0], bottom_left[1] + (top_left[1] - bottom_left[1]) / 2)
+            top_middle = (top_left[0] + (top_right[0] - top_left[0]) / 2, top_left[1])
+            bottom_middle = (bottom_left[0] + (bottom_right[0] - bottom_left[0]) / 2, bottom_left[1])
 
-        middle_right = (top_right[0], bottom_right[1] + (top_right[1] - bottom_right[1]) / 2)
-        middle_left = (top_left[0], bottom_left[1] + (top_left[1] - bottom_left[1]) / 2)
-        top_middle = (top_left[0] + (top_right[0] - top_left[0]) / 2, top_left[1])
-        bottom_middle = (bottom_left[0] + (bottom_right[0] - bottom_left[0]) / 2, bottom_left[1])
+            middle = (top_middle[0], middle_left[1])
 
-        middle = (top_middle[0], middle_left[1])
+            for i in range(input1.getNumPages()):
+                page1 = input1.getPage(i)
+                page1.mediaBox.upperLeft = top_left
+                page1.mediaBox.upperRight = top_middle
+                page1.mediaBox.lowerLeft = middle_left
+                page1.mediaBox.lowerRight = middle
 
-        for i in range(0, pages):
-            page1 = input1.getPage(i)
-            page1.mediaBox.upperLeft = top_left
-            page1.mediaBox.upperRight = top_middle
-            page1.mediaBox.lowerLeft = middle_left
-            page1.mediaBox.lowerRight = middle
+                page2 = input2.getPage(i)
+                page2.mediaBox.upperLeft = top_middle
+                page2.mediaBox.upperRight = top_right
+                page2.mediaBox.lowerLeft = middle
+                page2.mediaBox.lowerRight = middle_right
 
-            page2 = input2.getPage(i)
-            page2.mediaBox.upperLeft = top_middle
-            page2.mediaBox.upperRight = top_right
-            page2.mediaBox.lowerLeft = middle
-            page2.mediaBox.lowerRight = middle_right
+                page3 = input3.getPage(i)
+                page3.mediaBox.upperLeft = middle_left
+                page3.mediaBox.upperRight = middle
+                page3.mediaBox.lowerLeft = bottom_left
+                page3.mediaBox.lowerRight = bottom_middle
 
-            page3 = input3.getPage(i)
-            page3.mediaBox.upperLeft = middle_left
-            page3.mediaBox.upperRight = middle
-            page3.mediaBox.lowerLeft = bottom_left
-            page3.mediaBox.lowerRight = bottom_middle
+                page4 = input4.getPage(i)
+                page4.mediaBox.upperLeft = middle
+                page4.mediaBox.upperRight = middle_right
+                page4.mediaBox.lowerLeft = bottom_middle
+                page4.mediaBox.lowerRight = bottom_right
 
-            page4 = input4.getPage(i)
-            page4.mediaBox.upperLeft = middle
-            page4.mediaBox.upperRight = middle_right
-            page4.mediaBox.lowerLeft = bottom_middle
-            page4.mediaBox.lowerRight = bottom_right
+                pages = [page1, page2, page3, page4]
 
-            if order is not default_order:
-                [output.addPage([page1, page2, page3, page4][i - 1]) for i in order]
-            elif upside_down:
-                output.addPage(page3)
-                output.addPage(page1)
-                output.addPage(page4)
-                output.addPage(page2)
-            else:
-                output.addPage(page1)
-                output.addPage(page2)
-                output.addPage(page3)
-                output.addPage(page4)
+                for i in order:
+                    output.addPage(pages[i-1])
 
-        output.write(output_stream)
-        output_stream.close()
+            output.write(output_stream)
